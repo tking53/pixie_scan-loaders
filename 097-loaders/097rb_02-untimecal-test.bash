5b34e6f55aef73ccb2d467893952a0229d8c464d
Thomas King <tking.91@gmail.com>
2016-10-04 14:38:17 -0400

    inital commit for 097-loaders

---
+++ 097-loaders/097rb_02-untimecal-test.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+configdir="/home/tieria/programs/pixie_scan/config/ornl/OLTFvandle2016/analysis/"
+#cmddir="/home/tking/programs/loaders/cmd-segmented"
+datadir="/home/tieria/097rb-data"
+
+name="097rb_02-notimecal"
+
+configfile="Config.xml.97rb-notimecal"
+
+
+ln -sf /home/tieria/programs/pixie_scan/pixie_ldf_c
+ln -sf $configdir$configfile Config.xml
+
+cmd="zero\n"
+#cmd=$cmd"file $datadir/097rb_02.ldf\n"
+#cmd=$cmd"go\n"
+
+for j in `seq 6 10`
+do
+cmd=$cmd"file $datadir/097rb_02-$j.ldf\n"
+cmd=$cmd"go\n"
+
+done
+
+cmd=$cmd"end\n"
+echo -e "$cmd"  | ./pixie_ldf_c $name
+ln -sf /home/tieria/programs/pixie_scan/start.cmd
